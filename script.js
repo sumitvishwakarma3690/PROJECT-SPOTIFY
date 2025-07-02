c842627b51e0b9a2113ece5a56270d594603af69
Sumit Vishwakarma <162994296+sumitvishwakarma3690@users.noreply.github.com>
2025-07-03 01:21:23 +0530

    working js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Let's write some javascript");
 
 // This function return all the songs from songs directory
+let currentSong = new Audio();
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
@@ -17,30 +19,55 @@ async function getSongs() {
   return songs;
 }
 
+// stops and play the music
+const playMusic = (track)=>{
+  currentSong.src = "/songs/" + track
+  currentSong.play();
+  play.src = "img/pause.svg"
+  
+
+}
+
 async function main() {
-  // get the list of all the songs
+
+  // Get the list of all the songs
   let songs = await getSongs();
   console.log(songs);
 
+  // Show all the songs in the playlist 
   let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
   for (const song of songs) {
     songUL.innerHTML = songUL.innerHTML + `<li>
                             <img class="invert" src="/img/music.svg" alt="">
                             <div class="info">
-                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>${song.replaceAll("%20", " ")}</div> 
                                 <div>Sumit</div>
                             </div>
                             <div class="playnow">
                                <span>Play Now</span> 
                                 <img class="invert" src="img/play-circle.svg" alt="">
-                            </div>
-     </li>`; 
-     
-     // ye line jo hai songs ko inner HTML me dalega aur usko sahi format me dikhane ke liye list aur replaceAll the use hua hai 
+                            </div></li>`; 
+     // ye line jo hai songs ko inner HTML me dalega aur usko sahi format me dikhane ke liye list aur replaceAll the use hua hai
   }
 
-  // play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
+  // Attach an event listener to each song 
+  Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+    e.addEventListener("click", element=>{
+      console.log(e.querySelector(".info").firstElementChild.innerHTML)
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+  })
+
+  // Attack an event listener to play, next and previous 
+  play.addEventListener("click", ()=>{
+    if(currentSong.paused) {
+      currentSong.play()
+      play.src = "img/pause.svg"
+    }
+    else{
+      currentSong.pause()
+      play.src = "img/play.svg"
+    }
+  })
 }
 main();
