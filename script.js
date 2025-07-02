bd69bf4973598d3c2c709f96e282b694d86a5ce9
Sumit Vishwakarma <162994296+sumitvishwakarma3690@users.noreply.github.com>
2025-07-02 23:51:53 +0530

    working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Let's write some javascript");
 
 // This function return all the songs from songs directory
 async function getSongs() {
-  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -11,7 +11,7 @@ async function getSongs() {
   for (let i = 0; i < as.length; i++) {
     const element = as[i];
     if (element.href.endsWith("mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]); // ye jo hai /songs ke baad ka jo bhi hai wo lega 
     }
   }
   return songs;
@@ -22,8 +22,25 @@ async function main() {
   let songs = await getSongs();
   console.log(songs);
 
+  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML + `<li>
+                            <img class="invert" src="/img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Sumit</div>
+                            </div>
+                            <div class="playnow">
+                               <span>Play Now</span> 
+                                <img class="invert" src="img/play-circle.svg" alt="">
+                            </div>
+     </li>`; 
+     
+     // ye line jo hai songs ko inner HTML me dalega aur usko sahi format me dikhane ke liye list aur replaceAll the use hua hai 
+  }
+
   // play the first song
-  //   var audio = new Audio(songs[0]);
-  //   audio.play();
+    var audio = new Audio(songs[0]);
+    audio.play();
 }
 main();
